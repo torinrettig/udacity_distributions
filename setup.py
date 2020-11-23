ab965f5e5c892b675141d3b8cc9681c3d29b05d6
Torin Rettig <starplatinum87@gmail.com>
2020-11-23 15:51:20 -0800

    First commit of all packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='str_udacity_distributions', 
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['str_udacity_distributions'],
+      author='Torin Rettig',
+      author_email='torin.rettig@gmail.com',
+      url='https://github.com/torinrettig/udacity_distributions',      
+      zip_safe=False)
